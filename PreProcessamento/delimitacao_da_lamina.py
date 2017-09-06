60b5b2cca3b9c4c512aae2ad50f045cbdc177c42
Daniel Costa <dluiscosta@gmail.com>
2017-09-06 15:18:31 -0300

    Implementada interface com a linha de comando. Ainda é necessário ajustar os parâmetros.

--- PreProcessamento/delimitacao_da_lamina.py
+++ PreProcessamento/delimitacao_da_lamina.py
@@ -1,6 +1,6 @@
-import sys
 import cv2
 import cv2.cv
+import argparse
 
 def show_image(image, image_name):
     y, x = image.shape[0:2]
@@ -9,32 +9,53 @@ def show_image(image, image_name):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+#Parser de parâmetros do script
+parser = argparse.ArgumentParser(description="Delimita a região circular da lâmina.")
+parser.add_argument("input", help="Nome do arquivo da imagem de entrada.", action="store")
+parser.add_argument("-s", "--salvar", help="Nome do arquivo da imagem de saída.", action="store")
+parser.add_argument("-a", "--analise", help="Modo de análise", action="store_true")
+args = parser.parse_args()
 
-input_filename = sys.argv[1] 
-if len(sys.argv) >= 3:
-    output_filename = sys.argv[2] 
-else:
-    output_filename = "lamina_" + input_filename
-
-
+#Abre a imagem de entrada
+input_filename = args.input
 image = cv2.imread(input_filename, cv2.IMREAD_GRAYSCALE)
 
+if args.analise:
+    image_original = image
+    
+image = cv2.GaussianBlur(image, (9,9), 2, 2)
+edges = cv2.Canny(image, 120, 0, 5)   
 
+if args.analise:
+    image_blurred = image
+    image_with_edges = cv2.cvtColor(image, cv2.cv.CV_GRAY2RGB)
+    image_with_edges[edges==255] = [0, 0, 255];
 
+#Aplica a transformada de Hough para detectar os círculos na imagem
 circles = cv2.HoughCircles(image, cv2.cv.CV_HOUGH_GRADIENT, 
                            1, 
-                           minDist=image.shape[0]/8,
-                           param2=100,
-                           minRadius=image.shape[0]/3,
-                           maxRadius=image.shape[0]/2
+                           minDist=image.shape[0]/100,
+                           param2=120,
+                           minRadius=0,
+                           maxRadius=0
                            )
 
-for i in range(0, len(circles[0])):
-    center =  (int(circles[0][i][0]), int(circles[0][i][1])) 
-    cv2.circle(image, center, int(circles[0][i][2]), (255, 0, 0), 3)
-show_image(image, "Circulos")
+if args.analise:
+    if circles is None:
+        print "Nenhum círculo foi encontrado."
+    else:
+        print "Foram encontrados " + str(len(circles[0])) + " círculos."        
+        image_with_circles = image
+        #Desenha e imprime as informações dos círculos
+        for i in range(0, len(circles[0])):
+            print circles[0][i]
+            center =  (int(circles[0][i][0]), int(circles[0][i][1])) 
+            cv2.circle(image_with_circles, center, 
+                       int(circles[0][i][2]), (255, 0, 0), 3)
 
-
-
-
-cv2.imwrite(output_filename, image)
+if args.analise:
+        
+    
+#Se especificado, salva a imagem de saída
+if args.salvar != None:
+    cv2.imwrite(args.salvar, image)
