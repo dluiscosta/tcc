bb5aa0f28627fed44405d7a523352df614540510
Daniel Costa <dluiscosta@gmail.com>
2017-09-11 22:20:39 -0300

    Implementei extração de componentes conexos, para poder garantir que a binarização por Otsu resulte em duas regiões (por meio da união das k-2 menores com as que a englobam). A execução desse procedimento em python resultou em extrema lentidão, o que a torna inviável.

--- delimitacao_otsu.py
+++ delimitacao_otsu.py
@@ -29,6 +29,37 @@ mask = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 if args.analise:
     steps.append(cv2.cvtColor(mask[1], cv2.cv.CV_GRAY2RGB))
 
+'''
+#Componentes conexos
+from Queue import Queue
+import numpy as np
+
+label = 0
+height, width = mask[1].shape
+componentes_conexos = np.zeros(mask[1].shape)
+f = Queue()
+            
+def expand(a, b):
+    if b[0] >= 0 and b[1] >= 0 and b[0] < height and b[1] < width:
+        if componentes_conexos[b] == 0 and mask[1][b] == mask[1][a]:
+            componentes_conexos[b] = label    
+            f.put(b)
+
+for x in range(0, width):
+    for y in range(0, height):
+        if componentes_conexos[y][x] == 0:
+            label += 1
+            f.put((y, x))
+            componentes_conexos[y][x] = label 
+            while f.empty() == False:
+                yi, xi = f.get()
+                expand((yi, xi), (yi+1, xi+1))
+                expand((yi, xi), (yi+1, xi-1))
+                expand((yi, xi), (yi-1, xi+1))
+                expand((yi, xi), (yi-1, xi-1))                    
+#Ficou super lentíssimo
+#Python eu te abomino!
+'''
 
 #Exibe os passos do procedimento
 if args.analise:
