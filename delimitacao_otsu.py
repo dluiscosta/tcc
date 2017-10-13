15702d07ed866510c98df5f02d5d2797153a1fd4
Daniel Costa <dluiscosta@gmail.com>
2017-10-13 15:46:53 -0300

    Adicionei duas subetapas opcionais na delimitacao_otsu.py: a remoção de regiões espúrias através da seleção da região com maior área do contorno (supostamente a lâmina) e exclusão das demais; o encaixe de um círculo sobre essa região, calculado através do centro de massa e da área do seu contorno.
    Desenvolvi o script para um experimento que varia a efetuação dessas duas subetapas adicionais, bem como a cor da imagem de entrada (RGB ou tons de cinza), e faz uma comparação para com a lâmina anotada, para todas as imagens que a tenham. A métrica programada no momento para quantificar a similaridade entre as duas regiões é a de pixels_perdidos + 3*pixels_em_excesso (além da normalização e inversão), pois pegar partes exteriores à lâmina, que influenciarão no histograma da próxima etapa de segmentação, é mais prejudicial do que perder partes da lâmina.
    
    Executei o experimento sobre 16 imagens (as atualmente com a lâmina anotada) e obtive o seguinte resultado:
    
    run experimentos/delimitacao01.py
    Cor azul, sem remocao de regioes espurias, sem encaixe de circulo: 0.989819827108
    Cor azul, sem remocao de regioes espurias, com encaixe de circulo: 0.989819827108
    Cor azul, com remocao de regioes espurias, sem encaixe de circulo: 0.990222394292
    Cor azul, com remocao de regioes espurias, com encaixe de circulo: 0.991808123767
    Cor verde, sem remocao de regioes espurias, sem encaixe de circulo: 0.972929263745
    Cor verde, sem remocao de regioes espurias, com encaixe de circulo: 0.972929263745
    Cor verde, com remocao de regioes espurias, sem encaixe de circulo: 0.982403961723
    Cor verde, com remocao de regioes espurias, com encaixe de circulo: 0.987729906312
    Cor vermelho, sem remocao de regioes espurias, sem encaixe de circulo: 0.978838603304
    Cor vermelho, sem remocao de regioes espurias, com encaixe de circulo: 0.978838603304
    Cor vermelho, com remocao de regioes espurias, sem encaixe de circulo: 0.979493843997
    Cor vermelho, com remocao de regioes espurias, com encaixe de circulo: 0.984024747288
    Cor cinza, sem remocao de regioes espurias, sem encaixe de circulo: 0.980399369761
    Cor cinza, sem remocao de regioes espurias, com encaixe de circulo: 0.980399369761
    Cor cinza, com remocao de regioes espurias, sem encaixe de circulo: 0.982655132828
    Cor cinza, com remocao de regioes espurias, com encaixe de circulo: 0.986788091716
    MELHOR MEDIA: 0.991808123767
    Cor azul, com remocao de regioes espurias, com encaixe de circulo.

--- delimitacao_otsu.py
+++ delimitacao_otsu.py
@@ -2,8 +2,9 @@ import cv2
 import cv2.cv
 import argparse
 from exibir_imagens import mostra_imagens
-    
-def delimitacao_otsu(imagem, analise=False):
+import numpy as np
+
+def delimitacao_otsu(imagem, analise=False, removeEspurias=True, encaixaCirculo=True):
     if analise:
         print "- Delimitacao por limiarizacao de Otsu -"
         mostra_imagens([imagem], "", ["Imagem de entrada, apenas um canal"])
@@ -15,6 +16,44 @@ def delimitacao_otsu(imagem, analise=False):
     if analise:
         mostra_imagens([binaria], "",
                        ["Imagem binaria limiarizada em " + str(limiar)])
+    
+    if removeEspurias:
+        #Remocao de regioes menores dentro ou fora da lamina
+        contours, hierarchy = cv2.findContours(binaria, 
+                                               cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_NONE)
+        lamina = max(contours, key=cv2.contourArea) #assume que a lamina eh a regiao
+        #com contorno de maior area
+        
+        binaria = np.zeros(binaria.shape, dtype=np.uint8)
+        cv2.drawContours(binaria, [lamina], 0, 255, -1)
+
+        if analise:
+            mostra_imagens([binaria], "",
+                       ["Apos remocao das regioes espurias"])
+            
+        if encaixaCirculo:
+            M = cv2.moments(lamina) 
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            centro = (x, y)            
+            import math
+            raio = int(math.sqrt(cv2.contourArea(lamina)/math.pi))
+            
+            im = None
+            if analise:
+                #Indice, sobre a regiao original, onde estao o centro e perimetro
+                #do circulo encaixados.
+                im = cv2.cvtColor(binaria, cv2.cv.CV_GRAY2BGR)
+                cv2.circle(im, centro, raio, (0, 0, 255), 3)
+                cv2.circle(im, centro, 10, (0, 0, 255), -1)
+                        
+            binaria = np.zeros(binaria.shape, dtype=np.uint8)
+            cv2.circle(binaria, centro, raio, 255, -1)
+            
+            if analise:
+                mostra_imagens([im, binaria], "Circulo encaixado")
+                
         
     return binaria
 
--- experimentos/delimitacao01.py
+++ experimentos/delimitacao01.py
@@ -1,13 +1,48 @@
 import base
 from delimitacao_otsu import delimitacao_otsu
 import numpy as np
+import cv2
 
-cores = ["azul", "verde", "vermelho"]
-for c in [0, 1, 2]: #canais BGR
-    notas = []
-    for im in base.base.get_imagens(lambda im: im.lamina is not None):
-        imagem = im.get_imagem()[:,:,c]
-        otsu = delimitacao_otsu(imagem)
-        notas.append(im.lamina.avaliar_lamina(otsu, analise=False, indice=im.indice))
-    media = np.mean(notas)
-    print "Cor " + str(cores[c]) + ": " + str(media)
+imagens = base.base.get_imagens(lambda im: im.lamina is not None and (im.indice == 5 or im.indice == 111))
+
+cores = ["azul", "verde", "vermelho", "cinza"]
+shape = (4, #cores
+         2, #remocao de regioes espurias
+         2, #encaixe de um circulo
+         len(imagens))
+notas = np.zeros(shape,dtype=float)
+
+for i in range(0, len(imagens)):
+    im = imagens[i]
+    original = im.get_imagem()
+    for c in [0, 1, 2, 3]:  #canais BGR e tons de cinza
+        if c == 3:
+            imagem = cv2.cvtColor(original, cv2.cv.CV_BGR2GRAY)
+        else:
+            imagem = original[:,:,c]
+        for re in [True, False]:
+            for ec in [True, False]:
+                otsu = delimitacao_otsu(imagem, analise=False,
+                                        removeEspurias=re,
+                                        encaixaCirculo=ec)
+                notas[c,int(re),int(ec),i] = im.lamina.avaliar_lamina(otsu, analise=False)
+
+maior_media = 0
+prep = ["sem", "com"]
+for c in [0, 1, 2, 3]: 
+    for re in [0, 1]:
+        for ec in [0, 1]:
+            media = np.mean(notas[c,re,ec,:])
+            if media > maior_media:
+                maior_media = media
+                par_ot = (c, re, ec)
+            print ("Cor " + cores[c] +
+                   ", " + prep[re] + " remocao de regioes espurias" +
+                   ", " + prep[ec] + " encaixe de circulo" +
+                   ": " + str(media))
+            
+print "MELHOR MEDIA: " + str(maior_media)
+print ("Cor " + cores[par_ot[0]] +
+       ", " + prep[par_ot[1]] + " remocao de regioes espurias" +
+       ", " + prep[par_ot[2]] + " encaixe de circulo" +
+       ".")
