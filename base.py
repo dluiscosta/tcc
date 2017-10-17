0acd00be0eeb8abf3f788283e2d23bd3f4018f30
Daniel Costa <dluiscosta@gmail.com>
2017-10-17 16:00:45 -0200

    Salvei as anotacoes.pkl por fora do modulo.
    Comecei a construir a funcao de avaliacao da segmentacao de um patch; ainda não está funcionando corretamente.

--- base.py
+++ base.py
@@ -139,6 +139,113 @@ class Patch:
                         cels.append(Celula(self.indice*1000 + ind, tipo[1], comp, bur))
                         ind = ind + 1
         self.celulas = cels
+        
+    def avaliar_patch(self, tentativa, analise = False):
+        if self.celulas is None:
+            raise Exception("As celulas nao foram anotadas!")
+        
+        contours, hierarchy = tentativa
+                
+        cel = None
+        def area_celula():
+            area = 0
+            for comp in cel.componentes:
+                area = area + cv2.contourArea(comp)
+            for bur in cel.buracos:
+                area = area - cv2.contourArea(bur)
+                
+            im3 = np.zeros(shape_patch, dtype="uint8")
+            cv2.drawContours(im3, cel.componentes, -1, [255], -1)
+            cv2.drawContours(im3, cel.componentes, -1, [255], 2)
+            cv2.drawContours(im3, cel.buracos, -1, [0], -1)
+            print (area, np.count_nonzero(im3))
+            area = np.count_nonzero(im3)
+            return area
+            
+        inf = shape_patch[0]*shape_patch[1] #virtualmente infinito
+        im1, im2 = np.zeros((2,) + shape_patch, dtype="uint8") #gera duas imagens pretas para comparacao
+        def diferenca(idx_cont):     
+            if hierarchy[0,idx_cont,3] != -1: #eh um buraco
+                return inf
+            
+            #Desenha a celula na primeira imagem
+            cv2.drawContours(im1, cel.componentes, -1, [255], -1)
+            cv2.drawContours(im1, cel.componentes, -1, [255], 2)
+            cv2.drawContours(im1, cel.buracos, -1, [0], -1)
+            
+            #Desenha a regiao na segunda imagem
+            cv2.drawContours(im2, contours, idx_cont, [255], -1) #preenchimento
+            cv2.drawContours(im2, contours, idx_cont, [255], 2) #borda
+            if hierarchy[0,idx_cont,2] != -1: #ha filho(s) -> buraco(s)
+               idx_filho = hierarchy[0,idx_cont,2] #primeiro filho
+               cv2.drawContours(im2, contours, idx_filho, [0], 3)  
+               while hierarchy[0,idx_filho,0] != -1: #itera pelos outros filhos
+                   idx_filho = hierarchy[0,idx_filho,0]
+                   cv2.drawContours(im2, contours, idx_filho, [0], 3)  
+            
+            #Calcula a area da diferenca entre as regioes (XOR nos pixels)
+            dif = np.count_nonzero(cv2.bitwise_xor(im1,im2))
+            
+            #Restaura as imagens pretas
+            cv2.drawContours(im1, cel.componentes, -1, [0], -1)
+            cv2.drawContours(im2, contours, idx_cont, [0], -1) #preenchimento
+            cv2.drawContours(im2, contours, idx_cont, [0], 2) #borda
+
+            return dif
+        
+        im_an = None
+        if analise:
+            im_an = np.zeros(shape_patch + (3,), dtype="uint8")
+            im_an[:,:,:] = 255 
+            cv2.drawContours(im_an, contours, -1, [200, 200, 200], -1)
+            cv2.drawContours(im_an, contours, -1, [200, 200, 200], 2)
+            
+        dif_acc = 0
+        for c in self.celulas: #para cada celula anotada
+            cel = c #atribue na variavel global para ser acessado pela funcao
+            
+            #procura a regiao (1 contorno externo = 1 regiao) que mais se assemelha a celula
+            reg = min(range(0, len(contours)), key=diferenca)
+            
+            #uma celula nao pode gerar mais penalidade que a sua area
+            dif = diferenca(reg)
+            area = area_celula()
+            if dif > area:
+                print "NO MATCH"
+                reg = -1 #nenhuma regiao correspondente
+                dif_acc = dif_acc + area #acumula a area
+            else:
+                dif_acc = dif_acc + dif #acumula a diferenca
+        
+            if analise:
+                color = None
+                if reg != -1: #se ha regiao correspondente
+                    color = [255, 150, 50] #azul
+                else:
+                    color = [0, 0, 255] #vermelho
+                    
+                #Desenha o contorno da celula
+                cv2.drawContours(im_an, cel.componentes, -1, color, 1)
+                cv2.drawContours(im_an, cel.buracos, -1, color, 1)
+   
+                if reg != -1:             
+                    #Calcula os centros de massa da celula e da regiao correspondente
+                    M = cv2.moments(cel.componentes[0]) 
+                    c_x = int(M["m10"] / M["m00"])
+                    c_y = int(M["m01"] / M["m00"])
+                    M = cv2.moments(contours[reg]) 
+                    r_x = int(M["m10"] / M["m00"])
+                    r_y = int(M["m01"] / M["m00"])
+                    
+                    print ((c_x, c_y), (r_x, r_y))
+                    
+                    #Traca uma linha entre eles
+                    cv2.line(im_an, (c_x, c_y), (r_x,r_y), [0, 0, 0], 2)
+
+        if analise:
+            mostra_imagens([im_an], "Regioes Correspondentes")
+                   
+        return dif_acc
                         
 class Imagem:
     def __init__(self, indice):
