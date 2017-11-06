8d6378ac26b78c7e074b9b2a96683173ae24e90d
Nayara <nayara.gerolomo@gmail.com>
2017-11-05 22:37:11 -0200

    adiciona mais métodos q extraem caracteristicas em cima do contorno da regiao

--- conta_celulas.py
+++ conta_celulas.py
@@ -1,9 +1,10 @@
 from modulos import *
+import extracao_caracteristicas as excar
+import numpy
 
 def conta_celulas(imagem, analise=False):
     im_l = do(imagem, analise=analise) #delimita a lamina
     im_f = rr(imagem, analise=analise) #remove ruidos
     im_b = limiarizacao(im_f, im_l, analise=analise) #limiariza o interior da lamina
     regioes = er(im_b, analise=analise, original=imagem) #extrai regioes
-    
     return regioes
