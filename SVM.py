6f133bae4c839c26c1abf61cd9b2d439b927077e
Nayara <nayara.gerolomo@gmail.com>
2017-11-22 04:08:40 -0200

    classifica as regioes da celula

--- SVM.py
+++ SVM.py
@@ -2,7 +2,9 @@
 from sklearn import svm
 import pickle
 import numpy
-
+import cv2
+from conta_celulas import conta_celulas
+from regiao import Regiao
 
 def svm_cells(X, y):
     svm_linear = svm.SVC()
@@ -19,7 +21,6 @@ def load_regioes_anotadas(file_name_pk):
         regioes_anotadas = pickle.load(open(file_name_pk,'rb'))
     except:
         regioes_anotadas = []
-    print(regioes_anotadas)
     X = []
     y = []
     for regiao in regioes_anotadas:
@@ -29,7 +30,33 @@ def load_regioes_anotadas(file_name_pk):
     y = numpy.array(y)
     return X, y
 
-# X, y = load_regioes_anotadas('lalala')
+def classifica_imagem(svm, file_name):
+    imagem = cv2.imread(file_name,cv2.IMREAD_COLOR)
+    regioes_encontradas = conta_celulas(imagem=imagem)
+
+    cv2.namedWindow('imagem', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('imagem', 600,600)
+
+    for regiao_encontrada in regioes_encontradas:
+        regiao = Regiao(regiao_encontrada)
+        regiao.extrai_caracteristicas()
+
+        X = [regiao.caracteristicas]
+
+        classe = svm_predict(svm, X)[0]
+
+        if classe == 'n':
+            cv2.drawContours(imagem, regiao.get_contorno(), -1, (0,255,0), 6)
+        elif classe == 'l':
+            cv2.drawContours(imagem, regiao.get_contorno(), -1, (255,0,0), 6)
+        else:
+            cv2.drawContours(imagem, regiao.get_contorno(), -1, (0,0,255), 6)
+
+    cv2.imshow('imagem', imagem)
+    k = cv2.waitKey(0)
+
+# X, y = load_regioes_anotadas('regioes_anotadas.p')
 # svm = svm_cells(X, y)
+# classifica_imagem(svm, "/home/nayara/Desktop/TCC/index_image/69.jpg")
 
 # print(svm_predict(svm, sample))
